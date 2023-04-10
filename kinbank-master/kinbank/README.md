33b0996f347661d65d3d4d12ea3614aa5455a1ba
Elena Wang <elenawzy@gmail.com>
2023-04-10 00:00:23 -0400

    upload all files

---
+++ kinbank-master/kinbank/README.md
@@ -0,0 +1,34 @@
+# kinbank/varikin
+
+## How to cite
+
+If you use these data please cite
+this dataset using the DOI of the [particular released version](../../releases/) you were using
+
+## Description
+
+
+## Statistics
+
+
+![Glottolog: 100%](https://img.shields.io/badge/Glottolog-100%25-brightgreen.svg "Glottolog: 100%")
+![Concepticon: 12%](https://img.shields.io/badge/Concepticon-12%25-red.svg "Concepticon: 12%")
+![Source: 99%](https://img.shields.io/badge/Source-99%25-brightgreen.svg "Source: 99%")
+
+- **Varieties:** 1,229
+- **Concepts:** 943
+- **Lexemes:** 210,903
+- **Sources:** 991
+- **Synonymy:** 1.34
+
+## Possible Improvements:
+
+
+
+- Entries missing sources: 1757/210903 (0.83%)
+
+## CLDF Datasets
+
+The following CLDF datasets are available in [cldf](cldf):
+
+- CLDF [Wordlist](https://github.com/cldf/cldf/tree/master/modules/Wordlist) at [cldf/cldf-metadata.json](cldf/cldf-metadata.json)
---
+++ kinbank-master/merge_collections.py
@@ -0,0 +1,87 @@
+import glob
+import os
+from shutil import move
+from pathlib import Path
+import pandas as pd
+
+# for combining bibtex files
+import pybtex.errors
+pybtex.errors.strict = False
+from pybtex import database
+
+def new_path(path):
+    return("/".join(Path(path).parts[2:]))
+
+def move_files(filenames):
+    new_filenames =  ['./' + new_path(f) for f in filenames]
+    for old, new in zip(filenames, new_filenames):
+        print(old + "  " + os.path.dirname(new))
+        if not os.path.isdir(os.path.dirname(new)):
+            Path(os.path.dirname(new)).mkdir(parents=True, exist_ok=True)
+        move(old, new)
+
+## Varikin
+print("Copying Varikin")
+varikin_files = glob.glob("collections/varikin/kinbank/raw/**/*.csv", recursive=True)
+move_files(varikin_files)
+
+## Parabank
+print("Copying Parabank")
+parabank_files = glob.glob("collections/parabank/kinbank/raw/**/*.csv", recursive=True)
+move_files(parabank_files)
+
+## Goeldi
+print("Copying Goeldi")
+goeldi_files = glob.glob("collections/goeldi/kinbank/raw/**/*.csv", recursive=True)
+move_files(goeldi_files)
+
+## Kinura
+print("Copying Kinura")
+kinura_files = glob.glob("collections/kinura/kinbank/raw/**/*.csv", recursive=True)
+move_files(kinura_files)
+
+### Combine metadata
+all_filenames = [
+    "collections/varikin/kinbank/etc/languages.csv",
+    "collections/parabank/kinbank/etc/languages.csv",
+    "collections/kinura/kinbank/etc/languages.csv",
+    "collections/goeldi/kinbank/etc/languages.csv"
+]
+
+#combine all files in the list
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+#export to csv
+combined_csv.to_csv("kinbank/etc/languages.csv", index=False, encoding='utf-8-sig')
+
+# combine concepts data
+all_concepts = [
+        "collections/varikin/kinbank/etc/concepts.csv",
+    "collections/parabank/kinbank/etc/concepts.csv",
+    "collections/kinura/kinbank/etc/concepts.csv",
+    "collections/goeldi/kinbank/etc/concepts.csv"
+]
+
+concepts = pd.concat([pd.read_csv(f) for f in all_concepts])
+
+# remove duplicates
+concepts = concepts.drop_duplicates(keep = "first")
+concepts['Concepticon_ID'] = concepts['Concepticon_ID'].apply(lambda x: x if pd.isnull(x) else str(int(x)))
+concepts.to_csv("kinbank/etc/concepts.csv", index=False, encoding='utf-8-sig', float_format="%.")
+
+# combine source.bib files
+all_bibs = [
+    "collections/varikin/kinbank/raw/sources.bib",
+    "collections/parabank/kinbank/raw/sources.bib",
+    "collections/kinura/kinbank/raw/sources.bib",
+    "collections/goeldi/kinbank/raw/sources.bib"
+]
+
+out_bib = ""
+for bib in all_bibs:
+    with open(bib) as infile:
+        bib_contents = infile.read()
+        out_bib += bib_contents
+
+# parse bibtex
+out_bib = database.parse_string(out_bib, bib_format= 'bibtex')
+out_bib.to_file('kinbank/raw/sources.bib', bib_format = 'bibtex')
